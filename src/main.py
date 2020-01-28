ae5ed1867d847fb9a4f29ff2069291314fe20393
greenfrog <greenfrog82@naver.com>
2020-01-28 19:42:10 +0900

    add change_working_directory function to set proper working direcotry when main.py is executed by crontab.

--- src/main.py
+++ src/main.py
@@ -7,8 +7,18 @@ from stock import get_stock_info
 SLACK_INCOMMING_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
 
 
+def change_working_directory():
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+
 if __name__ == '__main__':
+    change_working_directory()
+
     _, stock_info = get_stock_info()
+    print('-----')
+    print(stock_info)
 
     headers = {
         'Content-Type': 'application/json',
