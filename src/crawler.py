dfb43847920c91b073c8c56489488a897d58e4ae
greenfrog <greenfrog82@naver.com>
2020-02-09 23:43:54 +0900

    crawling state pension information and double buying information was modified to use instituion info basically.

--- src/crawler.py
+++ src/crawler.py
@@ -13,8 +13,26 @@ KOSPI = (3, 'kospi')
 KOSDAQ = (5, 'kosdaq')
 
 INSTITUTION_FILE_NAME = 'institution.csv'
+STATE_PENSION_FILE_NAME = 'state_pension.csv'
 FOREIGNER_FILE_NAME = 'foreigner.csv'
 
+INSTITUION_INVESTER_CODE = '7050'
+STATE_PENSION_INVESTOR_CODE = '6000'
+FOREIGNER_INVESTER_CODE = '9000'
+
+
+def _parse_stock_information_by_class(chrome_driver, invester_element, invester_code, market, download_path, data_file_name, wait_for_sleep=6):
+    invester_element.select_by_value(invester_code)
+
+    chrome_driver.find_element_by_class_name('btn-board.btn-board-search').click()
+    excel_button = chrome_driver.find_element_by_xpath("//*[contains(text(), 'CSV')]")
+    excel_button.click()
+
+    time.sleep(wait_for_sleep)
+    file_path = f'{download_path}/{market[1]}_{data_file_name}'
+    os.rename(f'{download_path}/data.csv', file_path)
+
+    return file_path
 
 def crawling_stock_info():
     if not DEBUG:
@@ -29,8 +47,8 @@ def crawling_stock_info():
         # change select date to today
         schdate = chrome_driver.find_element_by_name('schdate')
         schdate.clear()
-        schdate.send_keys(datetime.now().strftime('%Y%m%d'))
-        # schdate.send_keys(datetime.now().strftime('20200130'))
+        # schdate.send_keys(datetime.now().strftime('%Y%m%d'))
+        schdate.send_keys(datetime.now().strftime('20200207'))
 
         stock_info_file_path_list = []
         for market in (KOSPI, KOSDAQ):
@@ -38,28 +56,13 @@ def crawling_stock_info():
             chrome_driver.find_element_by_css_selector(f'.design-fieldset > form > dl > dd > input:nth-child({market[0]})').click()
 
             select_element_id = chrome_driver.find_element_by_name('var_invr_cd').get_attribute("id")
+            invester_element = Select(chrome_driver.find_element_by_id(select_element_id))
             # 기관 합계
-            invester = Select(chrome_driver.find_element_by_id(select_element_id))
-            invester.select_by_value('7050')
-
-            chrome_driver.find_element_by_class_name('btn-board.btn-board-search').click()
-            excel_button = chrome_driver.find_element_by_xpath("//*[contains(text(), 'CSV')]")
-            excel_button.click()
-
-            time.sleep(6)
-            institution_file_path = f'{download_path}/{market[1]}_{INSTITUTION_FILE_NAME}'
-            os.rename(f'{download_path}/data.csv', institution_file_path)
-            stock_info_file_path_list.append(institution_file_path)
-
+            stock_info_file_path_list.append(_parse_stock_information_by_class(chrome_driver, invester_element, INSTITUION_INVESTER_CODE, market, download_path, INSTITUTION_FILE_NAME))
+            # 연기금
+            stock_info_file_path_list.append(_parse_stock_information_by_class(chrome_driver, invester_element, STATE_PENSION_INVESTOR_CODE, market, download_path, STATE_PENSION_FILE_NAME))
             # 외국
-            invester.select_by_value('9000')
-            chrome_driver.find_element_by_class_name('btn-board.btn-board-search').click()
-            excel_button.click()
-
-            time.sleep(6)
-            foreigner_file_path = f'{download_path}/{market[1]}_{FOREIGNER_FILE_NAME}'
-            os.rename(f'{download_path}/data.csv', foreigner_file_path)
-            stock_info_file_path_list.append(foreigner_file_path)
+            stock_info_file_path_list.append(_parse_stock_information_by_class(chrome_driver, invester_element, FOREIGNER_INVESTER_CODE, market, download_path, FOREIGNER_FILE_NAME))
 
         chrome_driver.close()
     else:
--- src/stock.py
+++ src/stock.py
@@ -6,10 +6,10 @@ from datetime import date
 
 from crawler import crawling_stock_info
 
-def parse_stock_info(stock_info_file_path, stock_info_msg_title):
-    dict_public_office = {}
-    file_public_office = open(stock_info_file_path, 'r')
-    csv_read = csv.reader(file_public_office)
+
+def _parse_simple_stock_info(stock_info_file_path):
+    dict_stock_info = {}
+    csv_read = csv.reader(open(stock_info_file_path, 'r'))
 
     first_row = True
     for row in csv_read:
@@ -17,11 +17,14 @@ def parse_stock_info(stock_info_file_path, stock_info_msg_title):
             first_row = False
             continue
         temp = {'name': row[1], 'purchase_amount': float(row[7].replace(",", ""))}
-        dict_public_office[row[0]] = temp
+        dict_stock_info[row[0]] = temp
 
+    return dict_stock_info 
+
+def parse_stock_info(stock_info_file_path, stock_info_msg_title):
+    dict_public_office = _parse_simple_stock_info(stock_info_file_path)
     stock_info_msg = ''
 
-    # 기관 자료 sort
     stock_info_msg += f'{stock_info_msg_title} TOP 5\n'
     sorted_public_office = OrderedDict(sorted(dict_public_office.items(), reverse=True, key=lambda x: (x[1]['purchase_amount'])))
 
@@ -32,19 +35,18 @@ def parse_stock_info(stock_info_file_path, stock_info_msg_title):
         stock_info_msg += f"{count}. {values['name']}\n"
         count+=1
 
-    #return stock_info_msg, sorted_public_office
     return stock_info_msg, sorted_public_office
 
-def get_double_buying_comapnies(foreigner_stock_info, public_office_stock_info):
+def get_double_buying_companies(foreigner_stock_info, public_office_stock_info):
     total_stock_info_msg = ''
     foreigner_stock_info_list = list(foreigner_stock_info.items())[:50]
     public_office_stock_info_list = list(public_office_stock_info.items())[:50]
-    
+
     count = 1
-    for f_item in foreigner_stock_info_list:
-        for p_item in public_office_stock_info_list:
-            if f_item[0] == p_item[0]:
-                total_stock_info_msg += f"{count}. {f_item[1]['name']}\n"
+    for p_item in public_office_stock_info_list:
+        for f_item in foreigner_stock_info_list:
+            if p_item[0] == f_item[0]:
+                total_stock_info_msg += f"{count}. {p_item[1]['name']}\n"
                 count += 1
                 break
 
@@ -58,7 +60,7 @@ def get_stock_info():
     today = date.today()
     stock_info_msg = f'{today.year}년 {today.month}월 {today.day}일 시간외 외인/기관 순매수 TOP 5 알림!!\n\n'
 
-    foreigner_stock_info_msg, foreigner_stock_info = parse_stock_info(stock_info_file_path_list[1], '외인')
+    foreigner_stock_info_msg, foreigner_stock_info = parse_stock_info(stock_info_file_path_list[2], '외인')
     public_office_stock_info_msg, public_office_stock_info = parse_stock_info(stock_info_file_path_list[0], '기관')
 
     stock_info_msg += '-- 코스피 --\n\n'
@@ -68,10 +70,10 @@ def get_stock_info():
 
     stock_info_msg += '\n'
     stock_info_msg += '50위권내의 양매수\n'
-    stock_info_msg += get_double_buying_comapnies(foreigner_stock_info, public_office_stock_info)
+    stock_info_msg += get_double_buying_companies(foreigner_stock_info, public_office_stock_info)
 
-    foreigner_stock_info_msg, foreigner_stock_info = parse_stock_info(stock_info_file_path_list[3], '외인')
-    public_office_stock_info_msg, public_office_stock_info = parse_stock_info(stock_info_file_path_list[2], '기관')
+    foreigner_stock_info_msg, foreigner_stock_info = parse_stock_info(stock_info_file_path_list[5], '외인')
+    public_office_stock_info_msg, public_office_stock_info = parse_stock_info(stock_info_file_path_list[3], '기관')
 
     stock_info_msg += '\n'
     stock_info_msg += '-- 코스닥 --\n\n'
@@ -80,7 +82,7 @@ def get_stock_info():
     stock_info_msg += public_office_stock_info_msg
     stock_info_msg += '\n'
     stock_info_msg += '50위권내의 양매수\n'
-    stock_info_msg += get_double_buying_comapnies(foreigner_stock_info, public_office_stock_info)
+    stock_info_msg += get_double_buying_companies(foreigner_stock_info, public_office_stock_info)
 
     return stock_info_msg 
 
